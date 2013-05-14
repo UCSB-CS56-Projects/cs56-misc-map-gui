7c0f3e58830b7ec3ecea3faec92b6c07c4914dc9
Bohan Lin <bohan_lin@yahoo.com>
2013-05-14 15:27:33 -0700

    added javadoc

--- src/thegui/Run.java
+++ src/thegui/Run.java
@@ -8,8 +8,8 @@ import java.io.IOException;
  * @author Aki Stankoski and Dennis Huynh
  */
 public class Run{
-public static void main(String[] args) throws IOException {
-TheGUI a = new TheGUI();
-a.setUpHomeScreen();
-}
+    public static void main(String[] args) throws IOException {
+	TheGUI a = new TheGUI();
+	a.setUpHomeScreen();//creates a new GUI named a and sets up the home screen that contains the buttons of all the locations with available directions
+    }
 }
