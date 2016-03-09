e5022411459ca25938826ed5842223948715f28a
Shouzhi Wan <shouzhiwan@umail.ucsb.edu>
2016-03-08 21:23:02 -0800

    search by Dept

--- src/edu/ucsb/cs56/projects/misc/map_gui/CommandLineVersion.java
+++ src/edu/ucsb/cs56/projects/misc/map_gui/CommandLineVersion.java
@@ -34,114 +34,113 @@ public class CommandLineVersion{
 		String t="Trailer";
 		Scanner reader;
 		int i;
-		String s;
+		String s = "";
 		while(yes){
-			reader=new Scanner(System.in);
-			System.out.println("enter 1 for department and 2 for specific building");
-			i=reader.nextInt();
-
-			if(i==1){
-			while(yes){
-				reader=new Scanner(System.in);
-				System.out.println("Which department are you interested in?\n"
-					+b+'\n'+e+'\n'+h+'\n'+l+'\n'+m+'\n'+p+'\n'+t+'\n');
-				s=reader.next();
-				if(s.equals(b)){
-
-				}
-				else if(s.equals(e)){
-
-				}
-				else if(s.equals(h)){
-
-				}
-				else if(s.equals(l)){
-
-				}
-				else if(s.equals(m)){
-
-				}
-				else if(s.equals(p)){
-
-				}
-				else if(s.equals(t)){
-
-				}
-				}
-
+		    reader=new Scanner(System.in);
+		    System.out.println("enter 1 for department and 2 for specific building");
+		    i=reader.nextInt();
+		    TheGUI gui = new TheGUI();
+		    if(i==1){
+		       
+			reader = new Scanner(System.in);
+			System.out.println("Which department are you interested in?\n"
+					   +b+'\n'+e+'\n'+h+'\n'+l+'\n'+m+'\n'+p+'\n'+t+'\n');
+			s = reader.next();
+			if(s.equals(b)){
+			    gui.BSIF();
 			}
+			else if(s.equals(e)){
+			    gui.ENGRSCI();
+			}
+			else if(s.equals(h)){
+			    gui.HSSB();
+			}
+			else if(s.equals(l)){
+			    gui.LIBRARY();
+			}
+			else if(s.equals(m)){
+			    gui.LLCH();
+			}
+			else if(s.equals(p)){
+			    gui.BRDA();
+			}
+			else if(s.equals(t)){
+			    gui.T387();
+			}
+			
+		    }
 			else if(i==2){
-				reader = new Scanner(System.in);
-				System.out.println("Which building");
-				System.out.println(MapStatics.bldgAbbrs);
-				s=reader.next();	
-				TheGUI gui=new TheGUI();
-				if(MapStatics.bldgAbbrs.contains(s)){
+			    reader = new Scanner(System.in);
+			    System.out.println("Which building");
+			    System.out.println(MapStatics.bldgAbbrs);
+			    s=reader.next();	
+			    //TheGUI gui=new TheGUI();
+			    if(MapStatics.bldgAbbrs.contains(s)){
 				
-					if(MapStatics.bldgAbbrs.indexOf(s)==0){
-						gui.T387();
+				if(MapStatics.bldgAbbrs.indexOf(s)==0){
+				    gui.T387();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==1){
+				    gui.T429();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==2){
+				    gui.BRDA();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==3){
+				    gui.BSIF();
 					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==1){
-						gui.T429();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==2){
-						gui.BRDA();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==3){
-						gui.BSIF();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==4){
-						gui.LIBRARY();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==5){
-						gui.ENGRSCI();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==6){
-						gui.ENGR2();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==7){
-						gui.GIRV();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==8){
-						gui.HFH();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==9){
-						gui.HSSB();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==10){
-						gui.KERR();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==11){
-						gui.LLCH();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==12){
-						gui.NORTH();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==13){
-						gui.PHELP();
-					}
-					else if(MapStatics.bldgAbbrs.indexOf(s)==14){
-						gui.SOUTH();
-					}
-
+				else if(MapStatics.bldgAbbrs.indexOf(s)==4){
+				    gui.LIBRARY();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==5){
+				    gui.ENGRSCI();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==6){
+				    gui.ENGR2();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==7){
+				    gui.GIRV();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==8){
+				    gui.HFH();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==9){
+				    gui.HSSB();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==10){
+				    gui.KERR();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==11){
+				    gui.LLCH();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==12){
+				    gui.NORTH();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==13){
+				    gui.PHELP();
+				}
+				else if(MapStatics.bldgAbbrs.indexOf(s)==14){
+				    gui.SOUTH();
 				}
 				
-				while(!(s.equals("yes")) && !(s.equals("no"))){
-					reader = new Scanner(System.in);
-					System.out.println("Continue? yes/no");
-					s=reader.next();
-				}
-
-				if(s.equals("yes")){
-					gui.frame.dispose();
-					continue;
-				}
-				if(s.equals("no")){
-					System.exit(0);
-				}
-
-			}
-	}
+			    }
+			}	
+		    while(!(s.equals("yes")) && !(s.equals("no"))){
+			reader = new Scanner(System.in);
+			System.out.println("Continue? yes/no");
+			s=reader.next();
+		    }
+		    
+		    if(s.equals("yes")){
+			gui.frame.dispose();
+			continue;
+		    }
+		    if(s.equals("no")){
+			System.exit(0);
+		    }
+		    
+		    
+		}
 
 		
 	}
